87da38ea1a83142a24aa20577c8d13625a8c5670
wangzhe <wangzhe1@richinfo.cn>
2024-07-19 23:06:15 +0800

    再次尝试推送

--- packages/play/src/main.ts
+++ packages/play/src/main.ts
@@ -2,6 +2,6 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 import { WlButton } from 'w-element';
-// console.log(WElement);
+console.log(WlButton);
 
 createApp(App).use(WlButton).mount('#app');
